706c9aa0698c2ab86ec6e85fcb0e626f7a8a7751
xprojects <infos@x-projects.de>
2026-09-08 10:29:51 +0200

    steps to Kondi Mode

--- Frontend/src/app/models/gaudi-mode.model.ts
+++ Frontend/src/app/models/gaudi-mode.model.ts
@@ -1,27 +1,45 @@
 export enum GaudiModeType {
     LOS = 'LOS',
     TEAM = 'TEAM',
+    TIME_COMBINATION = 'TIME_COMBINATION',
+    POINTS_COMBINATION = 'POINTS_COMBINATION',
 }
 
 export const GaudiModeTypeLabels: Record<GaudiModeType, string> = {
     [GaudiModeType.LOS]: 'Los-Modus',
     [GaudiModeType.TEAM]: 'Mannschaftswertung',
+    [GaudiModeType.TIME_COMBINATION]: 'Zeit-Kombination',
+    [GaudiModeType.POINTS_COMBINATION]: 'Punkte-Mischwertung',
 };
+
+export interface GaudiModeRaceEntry {
+    raceId: number;
+    weight?: number;
+}
+
+export interface GaudiModeRace {
+    raceId: number;
+    raceName: string;
+    weight: number;
+    sortOrder: number;
+}
 
 export interface GaudiMode {
     id: number;
-    raceId: number;
+    races: GaudiModeRace[];
     type: GaudiModeType;
     name: string;
     teamSize?: number;
+    pointsScaleId?: number;
     createdAt: string;
 }
 
 export interface GaudiModeRequest {
-    raceId: number;
+    races: GaudiModeRaceEntry[];
     type: GaudiModeType;
     name: string;
     teamSize?: number;
+    pointsScaleId?: number;
 }
 
 export interface GaudiLosPairing {
@@ -32,6 +50,16 @@ export interface GaudiLosPairing {
     participant2Name?: string;
 }
 
+export interface GaudiRankingLeg {
+    raceId: number;
+    raceName: string;
+    rawValue?: number;
+    penalty?: number;
+    adjustedValue?: number;
+    place?: number;
+    points?: number;
+}
+
 export interface GaudiRankingEntry {
     place: number;
     label: string;
@@ -40,4 +68,6 @@ export interface GaudiRankingEntry {
     valueMs?: number;
     referenceMs?: number;
     diffMs?: number;
+    totalPoints?: number;
+    legs?: GaudiRankingLeg[];
 }
