8b6bd9fd62988211b94ef873fcfdf71fdba3a62b
Benjamin Hummel <infos@x-projects.de>
2026-09-13 21:23:28 +0200

    adding pagination

---
+++ Frontend/src/app/store/version/version.selectors.ts
@@ -0,0 +1,19 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {VersionState} from './version.reducer';
+
+export const selectVersionState = createFeatureSelector<VersionState>('version');
+
+export const selectVersion = createSelector(
+    selectVersionState,
+    state => state.version
+);
+
+export const selectVersionLoading = createSelector(
+    selectVersionState,
+    state => state.loading
+);
+
+export const selectVersionError = createSelector(
+    selectVersionState,
+    state => state.error
+);
