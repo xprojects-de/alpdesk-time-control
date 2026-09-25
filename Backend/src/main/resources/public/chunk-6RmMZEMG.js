1cb9b8ee0099d90f989623e1a04d51c5e4bcb60d
Benjamin Hummel <infos@x-projects.de>
2026-09-25 18:43:31 +0200

    use shadowJar and make raceNumber and Age optional to PDF Output

---
+++ Backend/src/main/resources/public/chunk-6RmMZEMG.js
@@ -0,0 +1,1 @@
+import{Nt as rc,_ as Fm}from"./chunk-D-PgdQ4d.js";var r=Fm(`team`);var c=rc(r,e=>e.teams);var m=rc(r,e=>e.loading);rc(r,e=>e.error);export{m as n,c as t};
---
+++ Backend/src/main/resources/public/chunk-Bjzc34D3.js
@@ -0,0 +1,1 @@
+import{Nt as rc,_ as Fm}from"./chunk-D-PgdQ4d.js";var r=Fm(`startGroupTemplate`);var p=rc(r,t=>t.templates);var l=rc(r,t=>t.loading);rc(r,t=>t.error);export{p as n,l as t};
